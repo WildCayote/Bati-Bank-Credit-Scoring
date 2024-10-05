d323f6f5bc549090350cf9663ebc9f5214d2afd0
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-05 21:20:41 +0300

    Feat: Implemented the numerical_distribution method

--- scripts/exploratory_data_analyzer.py
+++ scripts/exploratory_data_analyzer.py
@@ -1,5 +1,7 @@
+import math
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 sns.set_theme()
 
@@ -48,6 +50,54 @@ class EDAAnalyzer:
         """
         A function that will give histogram plots of numerical data with a density curve that shows the distribution of data
         """
+        
+        # determine the numerical columns and data
+        numerical_data = self.data._get_numeric_data()
+        numerical_cols = numerical_data.columns
+
+        # detrmine number of rows and columns for 
+        num_cols = math.ceil(len(numerical_cols) ** 0.5)
+
+        # calculate the number of rows
+        num_rows = math.ceil(len(numerical_cols) / num_cols)
+
+        # create subpltos
+        fig, axes = plt.subplots(ncols=num_cols, nrows=num_rows, figsize=(14, 9))
+
+        # flatten the axes
+        axes = axes.flatten()
+
+        for idx, column in enumerate(numerical_cols):
+            # calculate the median and mean to use in the plots
+            median = self.data[column].median()
+            mean = self.data[column].mean()
+
+            # plot the histplot for that column with a density curve overlayed on it
+            sns.histplot(self.data[column], bins=15, kde=True, ax=axes[idx])
+
+            # add title for the subplot
+            axes[idx].set_title(f"Distribution plot of {column}", fontsize=10)
+
+            # set the x and y labels
+            axes[idx].set_xlabel(column, fontsize=9)
+            axes[idx].set_ylabel("Frequency", fontsize=9)
+
+            # add a lines for indicating the mean and median for the distribution
+            axes[idx].axvline(mean, color='black', linewidth=1) # the line to indicate the mean
+            axes[idx].axvline(median, color='red', linewidth=1) # the line to indivate the median 
+
+            # add legends for the mean and median
+            axes[idx].legend({'Median': median, 'Mean': mean})
+
+        # remove unused subplots
+        for unused in range(idx + 1, len(axes)):
+            plt.delaxes(ax=axes[unused])
+        
+        # create a tight layout
+        plt.tight_layout()
+
+        # show the plot
+        plt.show()
 
     def categorical_distribution(self):
         """
