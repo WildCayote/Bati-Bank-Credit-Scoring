512e92644f04964218f92bd585487f4b52161a1f
Tinsae Shemalise <tinsaeshemalise41@gmail.com>
2024-10-08 12:20:49 +0300

    Feat: Implemented the method score_rfms

--- scripts/credit_risk_modeler.py
+++ scripts/credit_risk_modeler.py
@@ -1,3 +1,4 @@
+from typing import List
 import pandas as pd
 
 
@@ -71,3 +72,45 @@ class CreditScoreEngine:
         rfms_data['Std_Deviation'] = rfms_data['Std_Deviation'].fillna(value=0)
 
         return rfms_data
+    
+    def score_rfms(self, rfms_data: pd.DataFrame, rfms_weights: List[float]=[0.1, 0.2, 0.5, 0.2]) -> pd.DataFrame:
+        """
+        A method that converts RFMS scores into one combined score. The process is as follows:
+        RFMS values will be scored 1 through 5,  5 being the highest and 1 being the lowest. The way this id done is by using quantiles as identifies of scores.
+        - Recency : 5 is given to the most recent visitors and 1 is given to least recent visitors.
+        - Frequency: 5 is given to users that have high frequency of transactions and 1 for those who don't
+        - Monetary: 5 is given to users that have high transaction amount and 1 for those who have low transaction amounts.
+        - Std_Deviation: 5 is given to users whoes transaction amounts are consistent and 1 for those that aren't.
+        
+        Now what is left is to combine the scores into one variable. Giving different importance to all of measures, i.e RFMS measures/scores. Here are the level of importance:
+        - Recency has the lowest importance with a weight of 10% on the overall combined score
+        - Monetary has the highest importance with a weight of 50% on the overall combined score
+        - Frequency and Std_Deviation have the same importance with both weighing 20% on the overall combined score
+
+        Args:
+            rfms_data(pd.DataFrame): the dataframe containing RFMS values in columns of their own.
+            rfms_weights(List[float]): the weights to be used for combining RFMS values to obatin a single score.(defautl 0.1, 0.2, 0.5, 0.2)
+
+        Returns:
+            pd.DataFrame: A dataframe containing the final score obtained from RFMS scores and the individual RFMS scores
+        """
+
+        # score the RFMS, 1 through 5
+        recency_score = pd.cut(x=rfms_data['Recency'], bins=5, labels=list(range(5, 0, -1)))
+        frequency_score = pd.cut(x=rfms_data['Frequency'], bins=5, labels=list(range(1, 6, 1)))
+        monetary_score = pd.cut(x=rfms_data['Monetary'], bins=5, labels=list(range(1, 6, 1)))
+        std_score = pd.cut(x=rfms_data['Std_Deviation'], bins=5, labels=list(range(5, 0, -1)))
+
+        # combine the scored RFMS using the provided weights
+        rfms_score = (recency_score.astype(int) * rfms_weights[0]) + (frequency_score.astype(int)) * rfms_weights[1] + (monetary_score.astype(int) * rfms_weights[2]) + (std_score.astype(int)) * rfms_weights[3]  
+
+        # create a dataframe to return the results
+        result = pd.DataFrame({
+            "RecencyScore": recency_score.astype(int),
+            "FrequencyScore": frequency_score.astype(int),
+            "MonetaryScore": monetary_score.astype(int),
+            "StdScore": std_score.astype(int),
+            "RFMS_Score": rfms_score
+        }).map(lambda x: float(x))
+
+        return result
